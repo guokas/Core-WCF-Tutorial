f50de1ca316dcf0375ced0a2d1123bfb7b29d130
guokas <guokaswu@gmail.com>
2024-07-08 22:41:34 +0800

    fix: core wcf tutorial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddServiceModelServices();
+builder.Services.AddServiceModelMetadata();
+//builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+builder.WebHost.ConfigureKestrel(ks =>
+{
+    ks.Listen(IPAddress.Any, 8080);
+    ks.Listen(IPAddress.Any, 8081, option =>
+    {
+        option.UseHttps();
+    });
+}).UseUrls("http://*:8080","https://*:8081");
+
+var app = builder.Build();
+
+app.UseServiceModel(serviceBuilder =>
+{
+    serviceBuilder.AddService<Service>(options=>
+    {
+        options.BaseAddresses.Add(new Uri("https://localhost:8081"));
+        options.BaseAddresses.Add(new Uri("http://localhost:8080"));
+    });
+    serviceBuilder.AddServiceEndpoint<Service, IService>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/Service.svc");
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpGetEnabled = true;
+    serviceMetadataBehavior.HttpsGetEnabled = true;
+});
+
+app.Run();
